67f9d4a1358dc8314f79d1abf50362a1db44af7f
kirinchen <a29243685@msn.com>
2016-08-10 17:27:49 +0800

    add lost files

---
+++ Dto/InboxTellObjectData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RFNEet {
+    public class InboxTellObjectData : InboxData {
+
+        public string oid;
+        public string tag;
+        private string _source;
+
+        public InboxTellObjectData() {
+            type = Type.ObjectMsg;
+        }
+
+        public string getSource() {
+            return _source;
+        }
+
+        internal void setSource(string s) {
+            _source = s;
+        }
+
+    }
+
+}
