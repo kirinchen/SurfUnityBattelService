f9693d42b84fb14ace8440b15fe0b60b0a266675
kirinchen <a29243685@msn.com>
2016-10-20 21:45:04 +0800

    RoomService is chnage to nomal object class

--- Rest/RoomService.cs
+++ Rest/RoomService.cs
@@ -4,68 +4,64 @@ using System;
 using Newtonsoft.Json;
 using BestHTTP;
 
-public class RoomService : MonoBehaviour {
+namespace RFNEet {
+    public class RoomService  {
+        
+        public delegate void OnFail(SurfMErrorDto sd, HTTPRequestStates s, HTTPResponse r, Exception e = null);
 
-    public delegate void OnFail(SurfMErrorDto sd, HTTPRequestStates s, HTTPResponse r, Exception e = null);
+        public static readonly string SUFFIX_CREATE_ROON = "/api/v1/room/";
+        public URestApi api;
 
-    public static readonly string SUFFIX_CREATE_ROON = "/api/v1/room/";
-    public URestApi api;
 
-    void Awake() {
-        if (api == null) {
-            api = GetComponent<URestApi>();
-        }
-    }
-
-    public void createRoom(CreateRoomData data,Action<string> onOK, OnFail eCb) {
-        api.postJson(SUFFIX_CREATE_ROON, data , onOK, (m, s, r) => {
-            handleError(m, s, r, eCb);
-        });
-    }
-
-    private void handleError(string m, HTTPRequestStates s, HTTPResponse r, OnFail eCb) {
-        try {
-            SurfMErrorDto d = SurfMErrorDto.parse(r.DataAsText);
-            eCb(d,s,r);
-        } catch (Exception e) {
-            eCb(null, s, r,e);
-        }
-    }
-
-    public class CreateRoomData {
-        public string gameKindUid;
-        public int maxPlayerCount;
-    }
-
-    public class SurfMErrorDto {
-        public long timestamp;
-        public int status;
-        public string error;
-        public string exception;
-        public string message;
-        public string path;
-
-        public enum SurfMError {
-            NONE 
+        public void createRoom(CreateRoomData data, Action<string> onOK, OnFail eCb) {
+            api.postJson(SUFFIX_CREATE_ROON, data, onOK, (m, s, r) => {
+                handleError(m, s, r, eCb);
+            });
         }
 
-        public bool isSuccess() {
-            return status >= 200 && status < 300;
+        private void handleError(string m, HTTPRequestStates s, HTTPResponse r, OnFail eCb) {
+            try {
+                SurfMErrorDto d = SurfMErrorDto.parse(r.DataAsText);
+                eCb(d, s, r);
+            } catch (Exception e) {
+                eCb(null, s, r, e);
+            }
         }
 
-        public SurfMError getSurfMError() {
-            return SurfMError.NONE;
+        public class CreateRoomData {
+            public string gameKindUid;
+            public int maxPlayerCount;
         }
 
-        public static SurfMErrorDto parse(string s) {
-            return JsonConvert.DeserializeObject<SurfMErrorDto>(s);
-        }
+        public class SurfMErrorDto {
+            public long timestamp;
+            public int status;
+            public string error;
+            public string exception;
+            public string message;
+            public string path;
 
-        public override string ToString() {
-            return JsonConvert.SerializeObject(this);
+            public enum SurfMError {
+                NONE
+            }
+
+            public bool isSuccess() {
+                return status >= 200 && status < 300;
+            }
+
+            public SurfMError getSurfMError() {
+                return SurfMError.NONE;
+            }
+
+            public static SurfMErrorDto parse(string s) {
+                return JsonConvert.DeserializeObject<SurfMErrorDto>(s);
+            }
+
+            public override string ToString() {
+                return JsonConvert.SerializeObject(this);
+            }
+
         }
 
     }
-
-
 }
