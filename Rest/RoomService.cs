3c8d36e370de6e3cf8a6f28387ee159f94e8ff71
kirinchen <a29243685@msn.com>
2016-10-22 23:37:16 +0800

    add query room by game kind method

--- Rest/RoomService.cs
+++ Rest/RoomService.cs
@@ -3,21 +3,33 @@ using System.Collections;
 using System;
 using Newtonsoft.Json;
 using BestHTTP;
+using System.Collections.Generic;
 
 namespace RFNEet {
     public class RoomService {
-
+        public delegate void QueryResult<T>( List<RoomInfo<T>> r);
         public delegate void OnFail(SurfMErrorDto sd, HTTPRequestStates s, HTTPResponse r, Exception e = null);
 
         public static readonly string SUFFIX_CREATE_ROON = "/api/v1/room/";
+        public static readonly string SUFFIX_QUERY_ROON = "/api/v1/room/{0}/query";
         internal URestApi api { get; private set; }
 
         public RoomService(URestApi a) {
             api = a;
         }
 
-        public void createRoom(CreateRoomData data, Action<string> onOK, OnFail eCb) {
+        public void create(CreateRoomData data, Action<string> onOK, OnFail eCb) {
             api.postJson(SUFFIX_CREATE_ROON, data, onOK, (m, s, r) => {
+                handleError(m, s, r, eCb);
+            });
+        }
+
+        public void query<T>(string gameKindUid, int pageIndex, int pageSize,object filter, QueryResult<T> qr, OnFail eCb)  {
+            string path = string.Format(SUFFIX_QUERY_ROON, gameKindUid);
+            api.postJson(path, filter, (msg)=> {
+                List<RoomInfo<T>> list = JsonConvert.DeserializeObject<List<RoomInfo<T>>>(msg);
+                qr(list);
+            }, (m, s, r) => {
                 handleError(m, s, r, eCb);
             });
         }
