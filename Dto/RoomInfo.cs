dd113da3de7462bfb6e275d181c1fb04bf33b3cc
kirinchen <a29243685@msn.com>
2016-10-24 16:02:32 +0800

    add lost RFNET files

---
+++ Dto/RoomInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace RFNEet {
+    public class RoomInfo<T>  {
+        public string roomId;
+        public string gameUid;
+        public int currentCount;
+        public int maxPlayerCount;
+        public T data;
+    }
+}
