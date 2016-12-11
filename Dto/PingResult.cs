a71c5c1b1561c0bd71d9f94b8b01486a6af1d8dd
kirinchen <a29243685@msn.com>
2016-12-11 14:17:21 +0800

    add lost file

---
+++ Dto/PingResult.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RFNEet {
+    public class PingResult {
+
+        public string timestamp;
+        public List<RoomInfo> list = new List<RoomInfo>();
+        public float p;
+
+        public class RoomInfo {
+            internal string wsUrl = "NONE";
+            public string roomId = "NONE";
+            public string gameUid = "NONE";
+            public int currentCount;
+            public int maxPlayerCount;
+        }
+
+    }
+}
+
+
+
+
