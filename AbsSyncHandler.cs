7c06478e8f605d26b532b72bd48985ab04639abd
kirinchen <a29243685@msn.com>
2016-12-11 13:51:29 +0800

    add abs sync handler

---
+++ AbsSyncHandler.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace RFNEet {
+    public abstract class AbsSyncHandler : MonoBehaviour, SyncHandler {
+        public enum Status {
+            Idle, Connecting
+        }
+
+        public Status state { get; private set; }
+        internal SyncCenter sc
+        {
+            get; private set;
+        }
+        internal Action<PingBundle> onPingError = (p) => { };
+
+        public void Awake() {
+            state = Status.Idle;
+            sc = GetComponent<SyncCenter>();
+        }
+
+        public void connect(PingBundle r) {
+            state = Status.Connecting;
+            try {
+                if (r.ok) {
+                    Debug.Log(r.bestRoom.wsUrl + " connected");
+                    sc.init(r.bestRoom.wsUrl, r.bestRoom.roomId, this);
+                    sc.connect(onConnected);
+                } else {
+                    onPingError(r);
+                }
+            } catch (Exception e) {
+                onPingError(r);
+            }
+        }
+
+        private List<Action> onConnectCbs = new List<Action>();
+        public void addOnConnectedCb(Action a) {
+            if (onConnectCbs == null) a(); else onConnectCbs.Add(a);
+        }
+        internal HandshakeDto handshakeDto { get; private set; }
+        internal virtual void onConnected(HandshakeDto hd, LocalPlayerRepo lpr) {
+            try {
+                handshakeDto = hd;
+                onConnectCbs.ForEach(a => { a(); });
+            } catch (Exception e) {
+                Debug.Log(e);
+            }
+        }
+
+        public abstract AllSyncData getCurrentInfoFunc(LocalPlayerRepo localRepo, bool hasCommData);
+
+        public virtual void onRemoteFirstSync(RemotePlayerRepo rpr, AllSyncData msg) {
+        }
+
+        private List<Action> onSelfInRoomCbs = new List<Action>();
+        internal void addOnSelfInRoomCb(Action a) { if (onSelfInRoomCbs == null) a(); else onSelfInRoomCbs.Add(a); }
+        public virtual void onSelfInRoom(LocalPlayerRepo lpr, Action inRoomMark) {
+            inRoomMark();
+            onSelfInRoomCbs.ForEach(a => { a(); });
+            onSelfInRoomCbs = null;
+            Debug.Log("onSelfInRoom inject ");
+        }
+
+        internal void addOnAllInRoomCb(Action a) { if (onAllInRoomCbs == null) a(); else onAllInRoomCbs.Add(a); }
+        private List<Action> onAllInRoomCbs = new List<Action>();
+        public virtual void onAllRemotePlayerReadyed(LocalPlayerRepo localRepo) {
+            onAllInRoomCbs.ForEach(a => { a(); });
+        }
+
+        public bool isAllInRoomed() {
+            return onAllInRoomCbs == null;
+        }
+
+        public abstract RemoteObject onNewRemoteObjectCreated(RemotePlayerRepo rpr, RemoteData rd);
+
+        public LocalObject handoverToMe(RemoteObject ro) {
+            return null;
+        }
+
+        public bool handoverToOther(RemoteObject ro) {
+            return false;
+        }
+
+        public void onConnectedClose(string msg) {
+        }
+
+        public void onServerShutdown(float cutTime) {
+        }
+
+
+    }
+}
---
+++ Rest/PingBundle.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using System;
+namespace RFNEet {
+    public class PingBundle {
+        internal float endTime;
+        internal bool ok;
+        internal bool done;
+        internal string serverName;
+        internal URestApi ua { get; private set; }
+        internal PingResult result;
+        internal float score;
+        internal PingResult.RoomInfo bestRoom = new PingResult.RoomInfo();
+        internal Action doneAction = () => { };
+        private bool calcRoomScore = true;
+        private string gameKindUid;
+
+        public PingBundle() {
+        }
+
+        public void reset() {
+            ok = false;
+            done = false;
+            serverName = string.Empty;
+            result = null;
+            bestRoom = new PingResult.RoomInfo();
+        }
+
+        public PingBundle(string gameKindUid, URestApi ua, bool calcRoomScore = true) {
+            this.gameKindUid = gameKindUid;
+            this.calcRoomScore = calcRoomScore;
+            this.ua = ua;
+            serverName = ua.name;
+        }
+
+        public void pingOne() {
+            string url = "/api/v1/rf/ping/" + gameKindUid + "?timestamp=" + Time.time;
+            Debug.Log(ua.host + " GO");
+            ua.postJson(url, null,
+             (s) => {
+                 correct(Time.time, s);
+             },
+            (m, s, r) => {
+                Debug.Log(ua.host + " No");
+                error(m);
+            }
+            );
+        }
+
+        internal void correct(float time, string s) {
+            ok = true;
+            endTime = time;
+            result = JsonConvert.DeserializeObject<PingResult>(s);
+            if (calcRoomScore) {
+                foreach (PingResult.RoomInfo r in result.list) {
+                    r.wsUrl = "ws://" + ua.host + ":" + ua.port + "/rfws";
+                }
+            }
+            setDone();
+            float ping = (endTime - float.Parse(result.timestamp));
+            Debug.Log("p=" + ping + " " + ua.name + "/" + ua.host + ":" + ua.port + " score=" + score + " OK  s=" + s);
+        }
+
+        public string genWsUrl() {
+            return "ws://" + ua.host + ":" + ua.port + "/rfws";
+        }
+
+        public void calcScore() {
+            if (ok) {
+                float st = (endTime - float.Parse(result.timestamp)) * 1000;
+                score = st;
+                if (calcRoomScore) {
+                    score += Mathf.Pow(result.list.Count, 2);
+                    score += getBestRoomScore();
+                }
+                score *= 1 + result.p;
+            } else {
+                score = 9999999;
+            }
+        }
+
+        private float getBestRoomScore() {
+            float s = 999999;
+            foreach (PingResult.RoomInfo r in result.list) {
+                float _s = getRoomScore(r);
+                if (_s < s) {
+                    s = _s;
+                    bestRoom = r;
+                }
+            }
+            return s;
+        }
+
+        private float getRoomScore(PingResult.RoomInfo r) {
+            int alertMax = (int)(r.maxPlayerCount * 0.8f);
+            if (r.currentCount > alertMax) {
+                return r.currentCount * 150;
+            } else {
+                int half = r.maxPlayerCount / 2;
+                return Mathf.Abs(r.currentCount - half);
+            }
+        }
+
+        internal void error(string s) {
+            ok = false;
+            setDone();
+        }
+
+        private void setDone() {
+            calcScore();
+            done = true;
+            doneAction();
+        }
+    }
+}
