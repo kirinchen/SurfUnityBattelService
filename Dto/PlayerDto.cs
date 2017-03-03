93f842a34bd3d8fd7924af553c42b1c7a49d2d2c
kirinchen <a29243685@msn.com>
2017-03-03 10:46:20 +0800

    add lost player dto files

---
+++ Dto/PlayerDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RFNEet {
+    public class PlayerDto  {
+        public string sessionId;
+        public int index;
+        public long startAt;
+    }
+}
