f84d402a2f47fe1ae009d67fac595797e8650ae7
kirinchen <a29243685@msn.com>
2017-08-28 12:36:22 +0800

    Leader board service

---
+++ LeaderboardService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RFNEet {
+    public class LeaderboardService : MonoBehaviour {
+        private URestApi rest;
+
+        void Awake() {
+            rest = GetComponent<URestApi>();
+        }
+
+        public void isRegistered() {
+            rest.get("/registered/{deviceId}",(s)=> {
+
+            },(e,s,r)=> {
+
+            });
+        }
+
+    }
+}
