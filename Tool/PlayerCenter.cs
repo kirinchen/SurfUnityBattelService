e7d58f9d59a1f55a2fe54aa92eb76a71504133e3
kirinchen <a29243685@msn.com>
2017-09-22 12:42:18 +0800

    refactory and add Player Cenetr

---
+++ Tool/PlayerCenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RFNEet {
+    public class PlayerCenter : MonoBehaviour {
+        public static PlayerCenter isntance { get; private set; }
+        private List<string> playerIds = new List<string>();
+        public string tokenPlayer { get; private set; }
+        private List<Action<string>> tokenPlayerChangeListeners = new List<Action<string>>();
+        private List<Action<string>> playerIntoListeners = new List<Action<string>>();
+
+
+
+        void Awake() {
+            isntance = this;
+        }
+
+        public void addPlayer(string id) {
+            playerIds.Add(id);
+            playerIntoListeners.ForEach(a=> { a(id); });
+        }
+
+        public void addPlayerIntoListeners(Action<string> a) {
+            playerIntoListeners.Add(a);
+        }
+
+        public void addTokenPlayerChangeListener(Action<string> a) {
+            tokenPlayerChangeListeners.Add(a);
+        }
+
+        public void setTokenPlayer(string id) {
+            if (!playerIds.Contains(id)) throw new NullReferenceException("not find this id=" + id);
+            tokenPlayer = id;
+            tokenPlayerChangeListeners.ForEach(a => { a(id); });
+        }
+
+
+
+
+    }
+}
