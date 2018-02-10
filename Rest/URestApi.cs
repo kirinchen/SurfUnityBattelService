62ec7e4e0df07f4892273f01244dc3d81fa393da
kirinchen <a29243685@msn.com>
2018-02-10 16:05:10 +0800

    remove show debug aution toke

--- Rest/URestApi.cs
+++ Rest/URestApi.cs
@@ -190,7 +190,6 @@ public class URestApi : MonoBehaviour {
     private void setupHeaders(HTTPRequest hr) {
         hr.AddHeader("charset", "utf-8");
         hr.AddHeader("Content-Type", "application/json");
-        Debug.Log("authorization=" + authorization);
         if (!string.IsNullOrEmpty(authorization)) {
             hr.AddHeader("Authorization", authorization);
         }
